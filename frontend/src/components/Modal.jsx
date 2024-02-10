a699943bc62da676ff9005699dea4c0ad07c8cad
Stormbreaker20 <luvsharma.del@gmail.com>
2024-02-10 14:48:57 +0530

    Dashboard

---
+++ frontend/src/components/Modal.jsx
@@ -0,0 +1,214 @@
+import React from "react";
+import * as Dialog from "@radix-ui/react-dialog";
+import { RxCross1 } from "react-icons/rx";
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+
+const Modal = ({ modal, setModal, data, setdata }) => {
+    
+    const [inpts,setInpts] = React.useState({
+    title:"",
+    date:"",
+    bill:"",
+    supplier:"",
+    qty:"",
+    amt:"",
+    desc:"",
+    cat:"",
+    section:"",
+    head:"",
+    fund:"",
+    payment:"",
+    type:"",
+    purpose:"",
+    })
+
+    const handleInputChange = (name, value) => {
+        setInpts((prevInpts) => ({
+          ...prevInpts,
+          [name]: value,
+        }));
+      };
+
+  return (
+    <Dialog.Root open={modal}>
+      {
+        <Dialog.Portal>
+          <Dialog.Overlay className="bg-black opacity-50 data-[state=open]:animate-overlayShow fixed inset-0 " />
+          <Dialog.Content
+            className={`data-[state=open]:animate-contentShow max-xl:min-w-[90vw] max-h-[700px] overflow-scroll overflow-x-hidden fixed min-w-[800px] left-[50%] top-[50%] flex translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none`}
+          >
+            <>
+              <div className="  rounded-lg bg-white h-full w-full pb-4 shadow-md">
+                <div className="border-b border-slate-200 pb-2 pl-5 pt-5 text-xl font-medium">
+                  Proposal Details
+                </div>
+                <div>
+                  <div className="mt-4 flex w-[100%] max-md:flex-col  gap-5 px-5 ">
+                    <div className="mb-5 md:w-1/2">
+                      <label
+                        htmlFor="title"
+                        className="text-md mb-2 block flex-grow   pl-1 font-medium text-gray-900 "
+                      >
+                        Title <span className="text-red-500">*</span>
+                      </label>
+                      <Input onChange={(e) => handleInputChange("title", e.target.value)} type="text"/>
+                    </div>
+                    <div className="mb-5 md:w-1/2">
+                      <label
+                        htmlFor="date"
+                        className="text-md mb-2 block w-1/2 pl-1 font-medium text-gray-900 "
+                      >
+                        Date <span className="text-red-500">*</span>
+                      </label>
+                      <Input onChange={(e) => handleInputChange("date", e.target.value)}  type="Date"/>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <div className="mt-4 flex w-[100%] max-lg:flex-col  gap-5 px-5 ">
+                    <div className="mb-5  md:max-w-[50%] flex-grow">
+                      <label
+                        htmlFor="bill"
+                        className="text-md mb-2 block pl-1 font-medium text-gray-900 "
+                      >
+                        Bill Number
+                      </label>
+                      <Input onChange={(e) => handleInputChange("bill", e.target.value)} type="Number"/>
+                    </div>
+                    <div className="mb-5 flex-grow">
+                      <label
+                        htmlFor="supplier"
+                        className="text-md mb-2 block  pl-1 font-medium text-gray-900 "
+                      >
+                        Supplier Name <span className="text-red-500">*</span>
+                      </label>
+                      <Input onChange={(e) => handleInputChange("supplier", e.target.value)} type="text"/>
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-4 flex w-[100%] max-lg:flex-col  gap-5 px-5 ">
+                  <div className="mb-5 flex-grow">
+                    <label
+                      htmlFor="qty"
+                      className="text-md mb-2 block  pl-1 font-medium text-gray-900 "
+                    >
+                      Quantity
+                    </label>
+                    <Input onChange={(e) => handleInputChange("qty", e.target.value)} type="Number"/>
+                  </div>
+                  <div className="mb-5 flex-grow">
+                    <label
+                      htmlFor="amt"
+                      className="text-md mb-2 block  pl-1 font-medium text-gray-900 "
+                    >
+                      Amount
+                    </label>
+                    <Input onChange={(e) => handleInputChange("amt", e.target.value)} type="Number"/>
+                  </div>
+                </div>
+                <div className="mt-4 w-[100%] flex-col  gap-5 px-5 ">
+                    <label
+                      htmlFor="qty"
+                      className="text-md mb-2 block  pl-1 font-medium text-gray-900 "
+                    >
+                      Description of Item
+                    </label>
+                    <Textarea onChange={(e) => handleInputChange("desc", e.target.value)} />
+                </div>
+                <div className="mt-4 flex w-[100%] max-lg:flex-col  gap-5 px-5 ">
+                  <div className="mb-5 flex-grow">
+                    <label
+                      htmlFor="cat"
+                      className="text-md mb-2 block  pl-1 font-medium text-gray-900 "
+                    >
+                      Sub Category
+                    </label>
+                    <Input onChange={(e) => handleInputChange("cat", e.target.value)} type="text"/>
+                  </div>
+                  <div className="mb-5 flex-grow">
+                    <label
+                      htmlFor="section"
+                      className="text-md mb-2 block  pl-1 font-medium text-gray-900 "
+                    >
+                      Section
+                    </label>
+                    <Input onChange={(e) => handleInputChange("section", e.target.value)} type="text"/>
+                  </div>
+                </div>
+                <div className="mt-4 flex w-[100%] max-lg:flex-col  gap-5 px-5 ">
+                  <div className="mb-5 flex-grow">
+                    <label
+                      htmlFor="head"
+                      className="text-md mb-2 block  pl-1 font-medium text-gray-900 "
+                    >
+                      Budget Head
+                    </label>
+                    <Input onChange={(e) => handleInputChange("head", e.target.value)} type="text"/>
+                  </div>
+                  <div className="mb-5 flex-grow">
+                    <label
+                      htmlFor="fund"
+                      className="text-md mb-2 block  pl-1 font-medium text-gray-900 "
+                    >
+                      Fund
+                    </label>
+                    <Input onChange={(e) => handleInputChange("fund", e.target.value)} type="text"/>
+                  </div>
+                </div>
+                <div className="mt-4 flex w-[100%] max-lg:flex-col  gap-5 px-5 ">
+                  <div className="mb-5 flex-grow">
+                    <label
+                      htmlFor="pass of payment"
+                      className="text-md mb-2 block  pl-1 font-medium text-gray-900 "
+                    >
+                      Passed of Payment
+                    </label>
+                    <Input onChange={(e) => handleInputChange("payment", e.target.value)} type="text"/>
+                  </div>
+                  <div className="mb-5 flex-grow">
+                    <label
+                      htmlFor="fund"
+                      className="text-md mb-2 block  pl-1 font-medium text-gray-900 "
+                    >
+                      Bill Settlement Type
+                    </label>
+                    <Input onChange={(e) => handleInputChange("type", e.target.value)} type="text"/>
+                  </div>
+                </div>
+                <div className="mt-4 w-[100%] flex-col  gap-5 px-5 ">
+                    <label
+                      htmlFor="qty"
+                      className="text-md mb-2 block  pl-1 font-medium text-gray-900 "
+                    >
+                      Purpose of Proposal
+                    </label>
+                    <Textarea onChange={(e) => handleInputChange("purpose", e.target.value)} />
+                </div>
+                <div className="flex w-[100%] max-lg:flex-col mt-5 md:justify-end justify-center items-center pr-5">
+                  <span className="flex  justify-center mx-5 cursor-pointer items-center gap-2 rounded-lg bg-[#0065C1] px-5 py-2 text-white shadow-md hover:shadow-blue-400">
+                    Save Changes
+                  </span>
+                </div>
+              </div>
+            </>
+
+            <Dialog.Close asChild>
+              <button
+                className="text-violet11 hover:bg-violet4 focus:shadow-violet7 absolute right-[0px] top-[5px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
+                aria-label="Close"
+                onClick={() => {
+                  setModal(false);
+                }}
+              >
+                <RxCross1 />
+              </button>
+            </Dialog.Close>
+          </Dialog.Content>
+        </Dialog.Portal>
+      }
+    </Dialog.Root>
+  );
+};
+
+export default Modal;
--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import Modal from "../components/Modal";
+import React from "react";
+import { useState } from "react";
 
 function dashboard() {
+  const Status = ["Create Proposal", "Pending", "In Review", "Past"];
+  const [status, setStatus] = useState(Status[1]);
+  const [modal, setModal] = useState(false);
+
+  const handleStatus = (val) => {
+    setStatus(val);
+  };    
+
+  const [data, setData] = useState([]);
+
+
   return (
-    <div>dashboard</div>
-  )
+    <>
+      <div className="p-5 flex flex-col gap-10">
+        <div className="flex gap-5">
+          <div
+            onClick={() => handleStatus(Status[1])}
+            className={`mx-1 flex h-[80px] ${
+              status === Status[1] ? "bg-blue-200" : ""
+            } w-[220px] cursor-pointer justify-between rounded-lg bg-white pl-3 pt-2 shadow-lg`}
+          >
+            <div className="flex flex-col gap-1 py-1 pl-2">
+              <span className=" text-2xl font-semibold text-slate-600">0</span>
+              <span className=" text-sm text-slate-400">{Status[1]}</span>
+            </div>
+          </div>
+          <div
+            onClick={() => handleStatus(Status[2])}
+            className={`mx-1 flex h-[80px] w-[220px] ${
+              status === Status[2] ? "bg-blue-200" : ""
+            } cursor-pointer justify-between rounded-lg bg-white pl-3 pt-2 shadow-lg`}
+          >
+            <div className="flex flex-col gap-1 py-1 pl-2">
+              <span className=" text-2xl font-semibold text-slate-600">0</span>
+              <span className=" text-sm text-slate-400">{Status[2]}</span>
+            </div>
+          </div>
+          <div
+            onClick={() => handleStatus(Status[3])}
+            className={`mx-1 flex h-[80px] w-[220px] ${
+              status === Status[3] ? "bg-blue-200" : ""
+            } cursor-pointer justify-between rounded-lg bg-white pl-3 pt-2 shadow-lg`}
+          >
+            <div className="flex flex-col gap-1 py-1 pl-2">
+              <span className=" text-2xl font-semibold text-slate-600">0</span>
+              <span className=" text-sm text-slate-400">{Status[3]}</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex h-[50vh] w-[100%] flex-col rounded-lg p-3 border bg-white shadow-lg">
+          <div className="flex w-[100%] px-5 py-4 text-xl ">
+            {status + " Proposals"}
+          </div>
+          <figure onClick={() => setModal(true)} className="flex flex-col items-center justify-center">
+            <img
+              width="22"
+              height="22"
+              className="mt-5 h-[200px] w-[200px]"
+              src="/images/nodata.gif"
+              alt=""
+            />
+            <figcaption className="text-[#2863C2]">No Data Found!</figcaption>
+          </figure>
+        </div>
+
+        <Modal modal={modal} setModal={setModal} />
+      </div>
+    </>
+  );
 }
 
-export default dashboard
+export default dashboard;
