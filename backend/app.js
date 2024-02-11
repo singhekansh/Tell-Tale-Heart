aa8d373691798dc537e4e3eab5913862f265d778
mostlyaman <amansharma110603@gmail.com>
2024-02-11 20:56:19 +0530

    add routers

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,6 @@
 const clubRouter = require('./club/club.router')
+const societyRouter = require('./society/society.router')
+const proposalRouter = require('./proposal/proposal.router')
 const express = require('express')
 const { authenticate } = require('./middleware')
 const admin = require('firebase-admin')
@@ -17,5 +19,7 @@ app.get('/', (req, res) => {
 
 app.use(authenticate)
 app.use('/club', clubRouter)
+app.use('/society', societyRouter)
+app.use('/proposal', proposalRouter)
 
 module.exports = app
